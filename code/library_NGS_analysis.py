143f1fb1955581d2ad2dd0c8cb5fae8e114443e5
Russell Walton <61611503+rtwalton@users.noreply.github.com>
2024-09-27 12:13:16 -0400

    more options for NGS design flexibility
    with R1 and R2 interleaved within a fastq, determine identity by mapping seed sequence to amplification primers

--- code/library_NGS_analysis.py
+++ code/library_NGS_analysis.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from Bio.Seq import Seq
 import gzip
 from tqdm import tqdm
-
+import constants
 
 
 tRNA_seq_dict = {
@@ -95,8 +95,8 @@ def extract_barcodes_from_fastq_pair(
 		read_1_ref_pos = 0
 		read_2_ref_pos = 0
 	else:
-		read_1_ref_pos = 21
-		read_2_ref_pos = 26
+		read_1_ref_pos = len(constants.read_1_primer_seq)
+		read_2_ref_pos = len(constants.read_2_primer_seq)
 
 	# first sgRNA spacer and iBAR in read 1
 	spacer1_coords = (read_1_ref_pos,read_1_ref_pos+20)
@@ -130,15 +130,26 @@ def extract_barcodes_from_fastq_pair(
 			infileR2.readline()
 		else:
 			# paried reads are alternating entries in single fastq
-			# read 2 is encoded by first line of fastq
-			read_sequenceR2 = infileR1.readline().strip()
+			read_sequence_1 = infileR1.readline().strip()
 			infileR1.readline()
 			infileR1.readline()
 			infileR1.readline()
 			# paired read 1 is subsequent entry
-			read_sequenceR1 = infileR1.readline().strip()
+			read_sequence_2 = infileR1.readline().strip()
 			infileR1.readline()
 			infileR1.readline()
+			# use first 8 nt of read to assign to read 1 or read 2
+			if (read_sequence_1[:8] == constants.read_1_primer_seq[:8]) or\
+				(read_sequence_2[:8] == constants.read_2_primer_seq[:8]):
+				read_sequenceR1 = read_sequence_1
+				read_sequenceR2 = read_sequence_2
+			elif (read_sequence_2[:8] == constants.read_1_primer_seq[:8]) or\
+				 (read_sequence_1[:8] == constants.read_2_primer_seq[:8]):
+				read_sequenceR1 = read_sequence_2
+				read_sequenceR2 = read_sequence_1
+			else: # for now, just defaulting to this option so that we still count the read as "not mapped"
+				read_sequenceR1 = read_sequence_1
+				read_sequenceR2 = read_sequence_2
 
 		sg1_seq = read_sequenceR1[spacer1_coords[0]:spacer1_coords[1]]
 		# iBARs in SBS orientation (reverse complement of sgRNA orientation)
@@ -173,11 +184,12 @@ def count_constructs(
 	iBAR2_UMI=False,
 	custom_read_primers = True,
 	single_fastq = False,
+	return_raw_barcodes=False,
 	max_reads=1e7):
 	
 	lib_design_df = generate_unique_construct_identifiers(lib_design_input_df, use_tRNA, iBAR2_UMI)
 
-	# df_total = pd.DataFrame()
+	df_total = pd.DataFrame()
 	df_summary = pd.DataFrame()
 
 	for index, row in tqdm(lib_info_df.iterrows(), total=lib_info_df.shape[0]):
@@ -213,8 +225,9 @@ def count_constructs(
 		df_barcodes['sample']=row['sample_ID']
 		df_barcodes['timepoint']=row['timepoint']
 		df_barcodes['replicate']=row['replicate']
-		
-		# df_total = pd.concat([df_total,df_barcodes])
+
+		if return_raw_barcodes:		
+			df_total = pd.concat([df_total,df_barcodes])
 
 		# add oligo counts to library design dataframe
 		if index==0:
@@ -293,5 +306,8 @@ def count_constructs(
 		df_summary.loc[row['sample_ID'], 'gini_coefficient'] =  gini(sublib_design_counts_df[row['sample_ID']].values)
 		df_summary.loc[row['sample_ID'], 'ratio_90_10'] = ratio_9010(sublib_design_counts_df[row['sample_ID']].values)
 
-	return lib_design_counts_df, df_summary
+	if return_raw_barcodes:
+		return lib_design_counts_df, df_summary, df_total
+	else:
+		return lib_design_counts_df, df_summary
 
