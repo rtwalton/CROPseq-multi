8be661f82d2c7a40d29c0168d56ab2d2c0902db0
rtwalton <russelltwalton@gmail.com>
2025-04-21 20:07:49 +0000

    improvements for analysis of multiple libraries in parallel, visualization

--- csm/library_NGS_analysis.py
+++ csm/library_NGS_analysis.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
 from Bio.Seq import Seq
 import gzip
 from tqdm import tqdm
@@ -282,13 +284,20 @@ def count_constructs(
     method = 'position', # or 'align' - either input fixed positions or try to align sequences
     max_reads=1e7):
     
-    lib_design_df = generate_unique_construct_identifiers(lib_design_input_df, use_tRNA, iBAR2_UMI)
+    # generate unique construct identifiers for library design if one design is passed as argument
+    if lib_design_input_df is not None:
+        lib_design_df = generate_unique_construct_identifiers(lib_design_input_df, use_tRNA, iBAR2_UMI)
 
     df_total = pd.DataFrame()
     df_summary = pd.DataFrame()
 
     for index, row in tqdm(lib_info_df.iterrows(), total=lib_info_df.shape[0]):
         
+        # generate unique construct identifiers for library design if construct design is not passed as argument
+        # and instead is specified in the library info file
+        if lib_design_input_df is None:
+            lib_design_df = generate_unique_construct_identifiers(pd.read_csv(row['design']), use_tRNA, iBAR2_UMI)
+
         if 'dialout' in lib_info_df.columns:
             lib_design_index = {
                 k:v for (v,k) in lib_design_df[ lib_design_df['dialout'] == row['dialout'] 
@@ -313,7 +322,7 @@ def count_constructs(
                             single_fastq = single_fastq,
                             max_reads=max_reads)
         else:
-            warnings.warn("'method' not recognized - will attempt to count construct by method 'align'")
+            warnings.warn("'method' not recognized - will attempt to count constructs by method 'align'")
             df_barcodes = extract_barcodes_from_fastq_pair_align(fastq_pair, 
                 use_tRNA=use_tRNA, iBAR2_UMI=iBAR2_UMI, 
                 single_fastq = single_fastq,
@@ -365,7 +374,9 @@ def count_constructs(
         # record summary statistics
         df_summary.loc[row['sample_ID'], 'timepoint'] = row['timepoint']
         df_summary.loc[row['sample_ID'], 'replicate'] = row['replicate']
+        df_summary.loc[row['sample_ID'], 'n_constructs'] = len(lib_design_df)
         df_summary.loc[row['sample_ID'], 'tot_reads'] = len(df_barcodes)
+        df_summary.loc[row['sample_ID'], 'NGS_coverage'] = len(df_barcodes)/len(lib_design_df)
         df_barcodes_mapped = df_barcodes[df_barcodes['design_index'].notna()]
 
         df_summary.loc[row['sample_ID'], 'spacer_1_map'] = df_barcodes['spacer_1_map'].sum()/len(df_barcodes)
@@ -418,3 +429,214 @@ def count_constructs(
     else:
         return lib_design_counts_df, df_summary
 
+
+def count_constructs_v2(
+    lib_info_df, 
+    use_tRNA=False, 
+    iBAR2_UMI=False,
+    custom_read_primers = True,
+    single_fastq = False,
+    return_raw_barcodes=False,
+    method = 'position', # or 'align' - either input fixed positions or try to align sequences
+    max_reads=1e7):
+    """
+    in this version, a path to the library design csv is passed as a column in the library info file
+    so that each sample in the library info file can have a different design
+
+    instead of returning a single design count dataframe, a dictionary of dataframes is returned
+    where the keys are the unique design names and the values are dataframes with counts for all samples 
+    that correspond to that design
+    """
+
+    df_total = pd.DataFrame()
+    design_count_dict = {
+        design:generate_unique_construct_identifiers(pd.read_csv(design), use_tRNA, iBAR2_UMI
+                                                     ) for design in lib_info_df['design'].unique()
+        }
+    df_summary = pd.DataFrame()
+
+    for index, row in tqdm(lib_info_df.iterrows(), total=lib_info_df.shape[0]):
+        
+        # generate unique construct identifiers for the library design
+        lib_design_df = generate_unique_construct_identifiers(pd.read_csv(row['design']), use_tRNA, iBAR2_UMI)
+
+        if 'dialout' in lib_info_df.columns:
+            lib_design_index = {
+                k:v for (v,k) in lib_design_df[ lib_design_df['dialout'] == row['dialout'] 
+                ]['unique_barcode_combo'].to_dict().items()
+            }
+        else:
+            lib_design_index = {
+                k:v for (v,k) in lib_design_df['unique_barcode_combo'].to_dict().items()
+            }
+
+        fastq_pair = [row['fastq_R1'], row['fastq_R2']]
+
+        if method == 'position':
+            df_barcodes = extract_barcodes_from_fastq_pair(fastq_pair, 
+                use_tRNA=use_tRNA, iBAR2_UMI=iBAR2_UMI, 
+                custom_read_primers = custom_read_primers,
+                single_fastq = single_fastq,
+                max_reads=max_reads)
+        elif method == 'align':
+            df_barcodes = extract_barcodes_from_fastq_pair_align(fastq_pair, 
+                            use_tRNA=use_tRNA, iBAR2_UMI=iBAR2_UMI, 
+                            single_fastq = single_fastq,
+                            max_reads=max_reads)
+        else:
+            warnings.warn("'method' not recognized - will attempt to count constructs by method 'align'")
+            df_barcodes = extract_barcodes_from_fastq_pair_align(fastq_pair, 
+                use_tRNA=use_tRNA, iBAR2_UMI=iBAR2_UMI, 
+                single_fastq = single_fastq,
+                max_reads=max_reads)
+        
+        # map individual elements
+        df_barcodes['spacer_1_map'] = df_barcodes['spacer_1'].isin(lib_design_df['spacer_1'])
+        df_barcodes['iBAR_1_map'] = df_barcodes['iBAR_1'].isin(lib_design_df['iBAR_1'])
+        df_barcodes['spacer_2_map'] = df_barcodes['spacer_2'].isin(lib_design_df['spacer_2'])
+        df_barcodes['iBAR_2_map'] = df_barcodes['iBAR_2'].isin(lib_design_df['iBAR_2'])
+        df_barcodes['tRNA_map'] = df_barcodes['tRNA'].notna()
+
+        # map full constructs
+        df_barcodes['design_index'] = df_barcodes['unique_barcode_combo'].map(lib_design_index)
+
+        df_barcodes['sample']=row['sample_ID']
+        df_barcodes['timepoint']=row['timepoint']
+        df_barcodes['replicate']=row['replicate']
+
+        if return_raw_barcodes:     
+            df_total = pd.concat([df_total,df_barcodes])
+
+        # add oligo counts to library design dataframe
+        design_count_dict[row['design']] = pd.merge(
+                design_count_dict[row['design']],
+                pd.Series(df_barcodes['design_index'].value_counts(),
+                    name=row['sample_ID']),
+                    left_index=True, right_index=True, how='outer').fillna(0)
+        if iBAR2_UMI:
+            design_count_dict[row['design']] = pd.merge(
+                design_count_dict[row['design']],
+                pd.Series(df_barcodes.groupby('design_index')['iBAR_2'].nunique(),
+                    name=row['sample_ID']+'_UMI'),
+                    left_index=True, right_index=True, how='outer').fillna(0)
+        
+        sublib_design_counts_df = design_count_dict[row['design']]
+        # record summary statistics
+        df_summary.loc[row['sample_ID'], 'timepoint'] = row['timepoint']
+        df_summary.loc[row['sample_ID'], 'replicate'] = row['replicate']
+        df_summary.loc[row['sample_ID'], 'n_constructs'] = len(lib_design_df)
+        df_summary.loc[row['sample_ID'], 'tot_reads'] = len(df_barcodes)
+        df_summary.loc[row['sample_ID'], 'NGS_coverage'] = len(df_barcodes)/len(lib_design_df)
+        df_barcodes_mapped = df_barcodes[df_barcodes['design_index'].notna()]
+
+        df_summary.loc[row['sample_ID'], 'spacer_1_map'] = df_barcodes['spacer_1_map'].sum()/len(df_barcodes)
+        df_summary.loc[row['sample_ID'], 'iBAR_1_map'] = df_barcodes['iBAR_1_map'].sum()/len(df_barcodes)
+        df_summary.loc[row['sample_ID'], 'spacer_2_map'] = df_barcodes['spacer_2_map'].sum()/len(df_barcodes)
+        df_summary.loc[row['sample_ID'], 'iBAR_2_map'] = df_barcodes['iBAR_2_map'].sum()/len(df_barcodes)
+        df_summary.loc[row['sample_ID'], 'tRNA_map'] = df_barcodes['tRNA_map'].sum()/len(df_barcodes)
+        
+        if iBAR2_UMI:
+            df_summary.loc[row['sample_ID'], 'iBAR_2_UMI'] = sublib_design_counts_df[row['sample_ID']+'_UMI'].mean()
+
+        if use_tRNA:
+            if iBAR2_UMI:
+                df_summary.loc[row['sample_ID'], 'all_elements_mapped'] =  (
+                    df_barcodes['spacer_1_map'] & df_barcodes['iBAR_1_map'] &\
+                    df_barcodes['tRNA_map'] &\
+                    df_barcodes['spacer_2_map']
+                    ).sum()
+            else:
+                df_summary.loc[row['sample_ID'], 'all_elements_mapped'] =  (
+                    df_barcodes['spacer_1_map'] & df_barcodes['iBAR_1_map'] &\
+                    df_barcodes['tRNA_map'] &\
+                    df_barcodes['spacer_2_map'] & df_barcodes['iBAR_2_map']
+                    ).sum()
+        else:
+            if iBAR2_UMI:
+                df_summary.loc[row['sample_ID'], 'all_elements_mapped'] =  (
+                    df_barcodes['spacer_1_map'] & df_barcodes['iBAR_1_map'] &\
+                    df_barcodes['spacer_2_map']
+                    ).sum()
+            else:
+                df_summary.loc[row['sample_ID'], 'all_elements_mapped'] =  (
+                    df_barcodes['spacer_1_map'] & df_barcodes['iBAR_1_map'] &\
+                    df_barcodes['spacer_2_map'] & df_barcodes['iBAR_2_map']
+                    ).sum()
+        
+        df_summary.loc[row['sample_ID'], 'mapped_constructs'] = len(df_barcodes_mapped)
+        # frequencies of mapping and recombination
+        df_summary.loc[row['sample_ID'], 'fraction_mapped'] = df_summary.loc[row['sample_ID'], 'mapped_constructs']/\
+            df_summary.loc[row['sample_ID'], 'tot_reads']
+        df_summary.loc[row['sample_ID'], 'fraction_recombined'] = 1 - \
+            df_summary.loc[row['sample_ID'], 'mapped_constructs'] / df_summary.loc[row['sample_ID'], 'all_elements_mapped']
+        # dropout and uniformity
+        df_summary.loc[row['sample_ID'], 'dropout_count'] = (sublib_design_counts_df[row['sample_ID']]==0).sum()
+        df_summary.loc[row['sample_ID'], 'gini_coefficient'] =  gini(sublib_design_counts_df[row['sample_ID']].values)
+        df_summary.loc[row['sample_ID'], 'ratio_90_10'] = ratio_9010(sublib_design_counts_df[row['sample_ID']].values)
+    
+    df_summary.index.rename('sample_ID', inplace=True)
+    if return_raw_barcodes:
+        return design_count_dict, df_summary, df_total
+    else:
+        return design_count_dict, df_summary
+
+
+def plot_summary_metrics(summary_df):
+    df_summary_melt = pd.melt(
+        summary_df.reset_index(), 
+        id_vars='sample_ID', 
+        value_vars=['spacer_1_map','iBAR_1_map','spacer_2_map','iBAR_2_map','fraction_mapped']
+        )
+    plt.figure(figsize=(1.5*len(summary_df),3))
+    sns.barplot(df_summary_melt, x='sample_ID', y='value', hue='variable', palette='tab20')
+    plt.ylabel('fraction of reads')
+    plt.ylim(0,1)
+    plt.legend(bbox_to_anchor=(1,1))
+    plt.show()
+
+    plt.figure(figsize=(0.5*len(summary_df),3))
+    sns.barplot(summary_df, x=summary_df.index, y='fraction_recombined')
+    # plt.ylim(0,1)
+    plt.ylim(0.0001,1)
+    plt.yscale('log')
+    plt.xticks(rotation=90)
+    plt.show()
+
+    plt.figure(figsize=(0.5*len(summary_df),3))
+    sns.barplot(summary_df, x=summary_df.index, y='gini_coefficient')
+    plt.xticks(rotation=90)
+    plt.show()
+
+    plt.figure(figsize=(0.5*len(summary_df),3))
+    sns.barplot(summary_df, x=summary_df.index, y='ratio_90_10')    
+    plt.xticks(rotation=90)
+    plt.show()
+
+def plot_lorenz_curves(samples_df, counts_dict, share_ax=True):
+    """
+    plot lorenz curves for all samples in summary_df
+    """
+
+    for ref, df in counts_dict.items():
+        samples = [s for s in df.columns if s in samples_df['sample_ID'].values]
+        for sample in samples:
+            lorenz_curve(df[sample].values, library_name=sample)
+            plt.title(sample)
+
+def plot_uniformity_histograms(samples_df, counts_dict):
+    """
+    plot histogram of gini coefficients for all samples in summary_df
+    """
+    for ref, df in counts_dict.items():
+        samples = [s for s in df.columns if s in samples_df['sample_ID'].values]
+        for sample in samples:
+            plt.figure(figsize=(3,3))
+            sns.histplot(
+                 df[sample], kde=True, 
+                 kde_kws={'bw_adjust': 2, 'cut': 0},
+                 color='dodgerblue',
+                 )
+            plt.title(sample)
+            plt.xlabel('reads per construct')
+            plt.ylabel('constructs')
+            plt.show()
